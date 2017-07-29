a69b6df861450b58feab078d708a3ad0ab48efa7
Rik Verstijnen <rik@rikverstijnen.nl>
2017-07-29 20:46:51 +0200

    Fix not switching directly to auto light modus
    When switching mode to "Auto" and conditions were right, light still did
    not switch to "Auto". An error occured when switching "S_Mediaspeler".

--- scripts/Mode.lua
+++ scripts/Mode.lua
@@ -9,7 +9,6 @@
         if (switch.state == 'On') then
             domoticz.log('Mode On')
 			domoticz.devices('Lights').switchSelector(10) --Auto
-			domoticz.devices('S_Mediaspeler').SwitchOn()
 			domoticz.devices('S_TV').SwitchOn()
 			domoticz.devices('S_DVD speler').SwitchOn()
 			domoticz.devices('S_Interactieve TV').SwitchOn()
@@ -18,21 +17,19 @@
 			--ToDo: Heating up
         elseif (switch.state == 'Auto') then
             domoticz.log('Mode Auto')		
+			if (domoticz.devices('Iemand thuis').state == 'On') then
+				domoticz.devices('Lights').switchSelector(10) --Auto
+				--ToDo: Heating up
+			end
 			--Devices on; even when not at home
-			domoticz.devices('S_Mediaspeler').SwitchOn()
 			domoticz.devices('S_TV').SwitchOn()
 			domoticz.devices('S_DVD speler').SwitchOn()
 			domoticz.devices('S_Interactieve TV').SwitchOn()
 			domoticz.devices('S_Wasmachine').SwitchOn()
 			domoticz.devices('S_Droger').SwitchOn()
-			if (domoticz.devices('Iemand thuis').state == 'On') then
-				domoticz.devices('Lights').switchSelector(10) --Auto
-				--ToDo: Heating up
-			end
 		elseif (switch.state == 'Off') then
             domoticz.log('Mode Off')
 			domoticz.devices('Lights').switchSelector(0) --Off
-			domoticz.devices('S_Mediaspeler').SwitchOff()
 			domoticz.devices('S_TV').SwitchOff()
 			domoticz.devices('S_DVD speler').SwitchOff()
 			domoticz.devices('S_Interactieve TV').SwitchOff()
